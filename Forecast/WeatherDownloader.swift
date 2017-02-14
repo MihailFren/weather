25a0746c141dceb2b84ccece787fbac3a930527f
mihail fren <mihailfren@gmail.com>
2017-02-14 20:02:29 +0300

    Скачиваем JSON

--- Forecast/WeatherDownloader.swift
+++ Forecast/WeatherDownloader.swift
@@ -35,3 +35,36 @@ class WeatherDownloader {
         
     }
 }
+
+class ForecastDownloader {
+    
+    
+    static let sharedInstance = WeatherDownloader()
+    
+    func requestWeather(latitude: Double, longitude:Double, comlition: @escaping (_ temprature:[Double]) -> ()) {
+        guard let url2 = URL(string: "http://samples.openweathermap.org/data/2.5/forecast?lat=\(latitude)&lon=\(longitude)&appid=0f1d0cf47de70dae65f65a41d213b0ec") else { return }
+        print(url2)
+        let session = URLSession(configuration: .default)
+        let dataTask = session.dataTask(with: url2) { (data, responce, error) in
+            if let data = data, error == nil {
+                guard let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? Dictionary<String, AnyObject> else { return }
+                print(json)
+                let list = json?["list"] as? Array<AnyObject>
+                print(list)
+//                let city = json?["name"] as? String
+//                let type = (json?["weather"] as? Array<Dictionary<String, AnyObject>>)?[0]["description"] as? String
+//                
+//                let temp = (json?["main"] as? Dictionary<String, AnyObject>)?["temp"] as? Double
+                
+                DispatchQueue.main.async {
+                    comlition([])
+                }
+            }
+        }
+        
+        dataTask.resume()
+        
+    }
+
+    
+}
